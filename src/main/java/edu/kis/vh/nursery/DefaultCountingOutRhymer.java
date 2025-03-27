e1d70fde601881de3015f1d389c2f8c4240d3de7
Artaresto <251207@edu.p.lodz.pl>
2025-03-27 14:59:09 +0100

    Zasotsowanie się do Stalych i adnotacji w Issues

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,11 +5,8 @@ public class DefaultCountingOutRhymer {
     public static final int INT = 12;
     public static final int INT1 = -1;
     public static final int INT2 = 11;
-<<<<<<< HEAD
-    private int[] NUMBERS = new int[INT];
-=======
     private int[] numbers = new int[INT];
->>>>>>> 496a3217b39dad10205b10200d24838eeb783b5f
+
 
     public int total = INT1;
 
@@ -29,21 +26,17 @@ public class DefaultCountingOutRhymer {
     protected final int peek() {
         if (isEmpty())
             return INT1;
-<<<<<<< HEAD
-        return NUMBERS[total];
-=======
+
         return numbers[total];
->>>>>>> 496a3217b39dad10205b10200d24838eeb783b5f
+
     }
 
     public int pop() {
         if (isEmpty())
             return INT1;
-<<<<<<< HEAD
-        return NUMBERS[total--];
-=======
+
         return numbers[total--];
->>>>>>> 496a3217b39dad10205b10200d24838eeb783b5f
+
     }
 
 }
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -7,7 +7,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public final int reportRejected() {
         return totalRejected;
     }
-<<<<<<< HEAD
+
         @Override
         public void push(int in) {
             if (!isEmpty() && in > peek())
@@ -15,13 +15,4 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             else
                 super.push(in);
         }
-=======
-    @Override
-    public void push(int in) {
-        if (!isEmpty() && in > peek())
-            totalRejected++;
-        else
-            super.push(in);
-    }
->>>>>>> 496a3217b39dad10205b10200d24838eeb783b5f
 }
